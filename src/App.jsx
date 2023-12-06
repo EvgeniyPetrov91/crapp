f10efeeb0e9833b55c440b626719924ec03040ac
Evgeniy Petrov <lito.8855@gmail.com>
2023-12-06 12:13:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+
+const Profile = () => {
+	return (
+	<h2>Это страница профиля</h2>
+	)
+}
+const Messages = () => {
+	return (
+		<h2>Страница с сообщениями</h2>
+	)
+}
+const Settings = () => {
+	return (
+		<h2>Это страница с настройками</h2>
+	)
+}
+
+function App() {
+	return (
+		<div className='container mt-5'>
+			<div className='row'>
+				<div className='col-3'>
+					<div className='nav flex-column nav-pills'>
+						<NavLink className='nav-link active' to='/'>
+							Главная
+						</NavLink>
+						<NavLink className='nav-link' to='profile'>
+							Профиль
+						</NavLink>
+						<NavLink className='nav-link' to='messages'>
+							Сообщения
+						</NavLink>
+						<NavLink className='nav-link' to='settings'>
+							Настройки
+						</NavLink>
+					</div>
+				</div>
+				<div className='col-9'>
+					<Routes>
+						<Route path='/' element={<h2>Выбирайте из меню</h2>}/>
+						<Route path='/profile' element={<Profile />} />
+						<Route path='messages' element={<Messages />}/>
+						<Route path='/settings' element={<Settings />} />
+					</Routes>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default App
